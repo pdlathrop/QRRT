4c62507fd45303eafb1bb11f4064796120140a08
Paul Lathrop <130102659+pdlathrop@users.noreply.github.com>
2023-04-12 11:25:23 -0700

    Update dec2vec.cpp

--- cpp_Implementation/dec2vec.cpp
+++ cpp_Implementation/dec2vec.cpp
@@ -4,6 +4,6 @@
 std::vector<int> dec2vec(int dec, int n) {
     assert(dec <= pow(2, n)-1 && "dec too big for register size n!");
     std::vector<int> phi(pow(2, n), 0);
-    phi[dec] = 1; // Compensate for MATLAB indexing with +1
+    phi[dec] = 1; 
     return phi;
 }
