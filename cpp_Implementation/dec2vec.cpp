be3b56b08f25665ac0bee9d5d10f743dd15006d6
Paul Lathrop <130102659+pdlathrop@users.noreply.github.com>
2023-12-04 11:10:45 -0800

    Delete cpp_Implementation directory

--- cpp_Implementation/dec2vec.cpp
+++
@@ -1,9 +0,0 @@
-#include <vector>
-
-// Function to convert a single decimal state to vector representation.
-std::vector<int> dec2vec(int dec, int n) {
-    assert(dec <= pow(2, n)-1 && "dec too big for register size n!");
-    std::vector<int> phi(pow(2, n), 0);
-    phi[dec] = 1; 
-    return phi;
-}
